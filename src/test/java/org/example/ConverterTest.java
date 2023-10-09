bdf5979e9367fb8f7590853b59de7889f9a0a144
Anastasiyatarazevich <anastasiya.tarazevich22@gmail.com>
2023-10-10 02:30:27 +0300

    TDD: написали тест, но он почему-то не работает. Ах да, мы же не написали метод в нашем классе, исправляемся

--- src/test/java/org/example/ConverterTest.java
+++ src/test/java/org/example/ConverterTest.java
@@ -2,11 +2,19 @@ package org.example;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ConverterTest {
 
     Converter converter = new Converter();
 
     @Test
     public void testSi() {
+        assertEquals(0.1, converter.si(1, 10));
+        assertEquals(17.62, converter.si(1, 1762));
+        assertEquals(3.3, converter.si(2, 33));
+        assertEquals(1111.1, converter.si(2, 11111));
+        assertEquals(500, converter.si(3, 0.5));
+        assertEquals(78000, converter.si(3, 78));
     }
 }
