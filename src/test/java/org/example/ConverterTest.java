fa73be721882ccd76a56e5c0070c0f6ea547a2ed
Marina <copperraspberry@yandex.ru>
2023-10-11 00:16:33 +0300

    feat: добавлен тест функции перевода из староруской системы единиц. Тест не запустился и выбросилась ошибка, поскольку функция отсутвует

--- src/test/java/org/example/ConverterTest.java
+++ src/test/java/org/example/ConverterTest.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.assertEquals;
 
 public class ConverterTest {
@@ -16,5 +18,20 @@ public class ConverterTest {
         assertEquals(1111.1, converter.si(2, 11111), 0.01);
         assertEquals(500, converter.si(3, 0.5), 0.01);
         assertEquals(78000, converter.si(3, 78), 0.01);
+    }
+
+    @Test
+    public void testOldRussian(){
+        assertEquals(BigDecimal.valueOf(0.11125), converter.oldRussian(1, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(0.445), converter.oldRussian(2, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(1.125), converter.oldRussian(3, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(6.2), converter.oldRussian(4, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(4.4), converter.oldRussian(5, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(0.775), converter.oldRussian(6, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(0.0625), converter.oldRussian(7, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(5.325), converter.oldRussian(8, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(2.275), converter.oldRussian(9, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(2667.5), converter.oldRussian(10, BigDecimal.valueOf(2.5)).stripTrailingZeros());
+        assertEquals(BigDecimal.valueOf(1.775), converter.oldRussian(11, BigDecimal.valueOf(2.5)).stripTrailingZeros());
     }
 }
